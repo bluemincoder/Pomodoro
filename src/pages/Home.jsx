1d1782545fc81f00452300a44c4a4d67fbf8a030
minaal07 <130892773+minaal07@users.noreply.github.com>
2024-12-11 20:04:23 +0530

    Integrated Spotify API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Timerclock from "../components/Timerclock";
+// import Spotify from 'react-spotify-embed';
+// import sp from "../components/Sp";
+import Iframe from 'react-iframe'
 
 function Home() {
     return (
         <div className="Home">
             <Timerclock />
+            <Iframe url="https://open.spotify.com/embed/track/6gjarl3YlvQtnvGCZQ4IRK?utm_source=generator"
+                 width="320px"
+                 height="160px"
+                 id=""
+                 className=""
+                 display="block"
+                 position="absolute"/>
         </div>
     );
 }
